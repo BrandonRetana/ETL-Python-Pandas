99ad004e31cbd5a652086871c7e0dca6651b36c2
BrandonRetana <branrecha140356@gmail.com>
2022-11-23 22:42:44 -0600

    df

---
+++ db.sql
@@ -0,0 +1,13 @@
+CREATE TABLE etl (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  delito varchar(255),
+  subdelito varchar(255),
+  fecha VARCHAR(255),
+  hora VARCHAR(255),  
+  victima VARCHAR(255),
+  subvictima VARCHAR(255),
+  edad VARCHAR(255),
+  genero VARCHAR(255),
+    nacionalidad VARCHAR(255),
+    provincia VARCHAR(255),
+) 
